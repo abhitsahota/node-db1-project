c1baef82f6a16dd1b2a21070a948f04687d8c4d8
abhitsahota <abhit.sahota@gmail.com>
2020-05-20 22:37:06 -0400

    completed MVP

--- api/server.js
+++ api/server.js
@@ -6,4 +6,55 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', async (req, res) => {
+    try {
+        const result = await db('accounts')
+        res.status(200).json({ msg: result })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'problem with db', error: err })
+    }
+})
+
+server.get('/api/accounts/:id', async (req, res) => {
+    try {
+        const result = await db('accounts').where({ id: req.params.id })
+        res.status(200).json({ msg: result })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'problem with db', error: err })
+    }
+})
+
+server.post('/api/accounts/', async (req, res) => {
+    try {
+        const result = await db('accounts').insert(req.body)
+        res.status(200).json({ msg: result })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'problem with db', error: err })
+    }
+})
+
+server.put('/api/accounts/:id', async (req, res) => {
+    try {
+        const result = await db('accounts').where({ id : req.params.id }).update(req.body)
+        res.status(200).json({ msg: result })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'problem with db', error: err })
+    }
+})
+
+server.delete('/api/accounts/:id', async (req, res) => {
+    try {
+        const result = await db('accounts').where({ id: req.params.id }).del()
+        res.status(200).json({ msg: result })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'problem with db', error: err })
+    }
+})
+
+
 module.exports = server;
